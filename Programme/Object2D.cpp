7b0f8c5f706a740b20799452996e12007d266bdd
bl0ggy <jessys34@hotmail.fr>
2016-11-24 21:57:07 +0100

    Added Object2D class.

---
+++ Programme/Object2D.cpp
@@ -0,0 +1,22 @@
+#include "Object2D.h"
+
+double Object2D::GetHeight()
+{
+    return m_height;
+}
+double Object2D::GetWidth()
+{
+    return m_width;
+}
+double Object2D::GetPositionX()
+{
+    return m_positionX;
+}
+double Object2D::GetPositionY()
+{
+    return m_positionY;
+}
+double Object2D::GetDirection()
+{
+    return m_direction;
+}
---
+++ Programme/Object2D.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class Object2D
+{
+public:
+    double GetHeight();
+    double GetWidth();
+    double GetPositionX();
+    double GetPositionY();
+    double GetDirection();
+private:
+    double m_height;
+    double m_width;
+    double m_positionX;
+    double m_positionY;
+    double m_direction;
+}
